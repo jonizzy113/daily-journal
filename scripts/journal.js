a98f01fb32317e46f9d9217515875b1160841810
Jonathan Schaffer <jonizzyschaffer@gmail.com>
2019-03-01 11:36:01 -0600

    Added jornal.js

---
+++ scripts/journal.js
@@ -0,0 +1,15 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const journalEntry = {
+    date: "07/02/1998",
+    concepts: "Javascript",
+    entry: "1",
+    mood: "shitty"
+}
+
+entry = []
+entry.push(journalEntry)
+
+console.table(entry)
